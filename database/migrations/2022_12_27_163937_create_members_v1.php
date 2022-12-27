a08ce57a45dcc6c1574b1f9a700f87d97a2950f9
Mario Work FOS <trabajo.makroz@gmail.com>
2022-12-27 15:59:37 -0400

    migrations and seed user test

--- database/migrations/2022_12_27_163937_create_members_v1.php
+++ database/migrations/2022_12_27_163937_create_members_v1.php
@@ -13,8 +13,51 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('members_v1', function (Blueprint $table) {
+        Schema::create('levels', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('colors')->nullable();
+            $table->integer('points')->default(0);
+            $table->tinyInteger('status')->default(1);
+        });
+
+        Schema::create('members', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('icn')->unique();
+            $table->string('pin',4);
+            $table->string('password');
+            $table->datetime('register_date');
+            $table->integer('points')->default(0);
+            $table->tinyInteger('status')->default(1);
+            $table->foreignId('level_id')->constrained()->default(1);
+            $table->foreignUuid('sponsor_id', 'members')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('duration')->default(30);
+            $table->tinyInteger('sequense')->default(1);
+            $table->integer('points')->default(0);
+            $table->tinyInteger('status')->default(1);
+            $table->foreignId('level_id')->constrained();
+            $table->timestamps();
+        });
+
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('to_date');
+            $table->datetime('executed_date')->nullable();
+            $table->string('meet_link')->nullable();
+            $table->integer('points')->default(0);
+            $table->tinyInteger('status')->default(1);
+            $table->foreignUuid('member_id')->constrained();
+            $table->foreignId('challenge_id')->constrained();
+            $table->foreignId('level_id')->constrained();
             $table->timestamps();
         });
     }
@@ -26,6 +69,9 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('members_v1');
+        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('challenges');
+        Schema::dropIfExists('members');
+        Schema::dropIfExists('levels');
     }
 };
