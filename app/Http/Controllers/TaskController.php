96fc39a328014a5f3fcd7c02652a9550e953399d
Mario Work FOS <trabajo.makroz@gmail.com>
2023-01-31 10:15:03 -0400

    linit request

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Live;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -13,6 +14,7 @@ class TaskController extends Controller
         DB::beginTransaction();
         try {
             $task = Task::where('id', $id)->where('status', 'O')->where('member_id', $request->user()->id)->update(['status' => 'S', 'start_date' => now()]);
+            Live::where('id', $request->live_id)->update(['cant_asist' => DB::raw('cant_asist + 1')]);
             DB::commit();
         } catch (\Exception $e) {
             DB::rollback();
@@ -26,6 +28,7 @@ class TaskController extends Controller
         DB::beginTransaction();
         try {
             $task = Task::where('id', $id)->where('status', 'C')->where('member_id', $request->user()->id)->update(['status' => 'E', 'ended_date' => now()]);
+            Live::where('id', $request->live_id)->update(['cant_aproved' => DB::raw('cant_aproved + 1')]);
             DB::commit();
         } catch (\Exception $e) {
             DB::rollback();
